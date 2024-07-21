980945a990c2477de061cd11ae20a5e71ed5a23e
breno-okra <brenookra@gmail.com>
2024-07-21 14:59:49 -0300

    add notifications toast from sonner's website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
 import './global.css'
+import { Toaster } from 'sonner'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 export function App() {
@@ -9,6 +10,7 @@ export function App() {
       {/* %s significa que vai ser uma variavel, um nome que vai ser diferente em cada pagina, o resto sera igual */}
       {/* chamando em cada pagina  <Helmet title='title' /> com seu proprio title */}
       <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors closeButton/>
       <RouterProvider router={router}/>
     </HelmetProvider>
     
