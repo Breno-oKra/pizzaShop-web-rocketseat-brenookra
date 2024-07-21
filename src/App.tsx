7f77134df5e0cc8c82c2d0f8836a4e7dba54cfe3
breno-okra <brenookra@gmail.com>
2024-07-21 13:20:35 -0300

    using react Helmet for change page title SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { RouterProvider } from 'react-router-dom'
 import './global.css'
 import { router } from './routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 export function App() {
 
   return (
-    <RouterProvider router={router}/>
+    <HelmetProvider>
+      {/* %s significa que vai ser uma variavel, um nome que vai ser diferente em cada pagina, o resto sera igual */}
+      {/* chamando em cada pagina  <Helmet title='title' /> com seu proprio title */}
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router}/>
+    </HelmetProvider>
+    
   )
 }
--- src/pages/auth/singIn.tsx
+++ src/pages/auth/singIn.tsx
@@ -1,3 +1,9 @@
-export function SingIn(){
-    return <h1>Hello User</h1>
+import { Helmet } from 'react-helmet-async'
+export function SingIn() {
+    return (
+        <>
+            <Helmet title='Login' />
+            <h1>Hello User</h1>
+        </>
+    )
 }
