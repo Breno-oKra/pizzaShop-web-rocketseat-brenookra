8633f33d50da941d5a6b7279bdba1610631751e0
breno-okra <brenookra@gmail.com>
2024-07-22 00:56:19 -0300

    add pie chart graph

--- src/pages/app/Dashboard/revenue-chart.tsx
+++ src/pages/app/Dashboard/revenue-chart.tsx
@@ -1,18 +1,18 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import {ResponsiveContainer,LineChart,XAxis,YAxis,CartesianGrid,Line,Tooltip} from "recharts"
+import { ResponsiveContainer, LineChart, XAxis, YAxis, CartesianGrid, Line, Tooltip } from "recharts"
 import colors from 'tailwindcss/colors'
 const data = [
-    {date:"10/12",revenue:200},
-    {date:"11/12",revenue:400},
-    {date:"12/12",revenue:100},
-    {date:"13/12",revenue:3200},
-    {date:"14/12",revenue:900},
-    {date:"15/01",revenue:15380},
-    {date:"22/01",revenue:3200},
-    {date:"29/01",revenue:12200},
+    { date: "10/12", revenue: 200 },
+    { date: "11/12", revenue: 400 },
+    { date: "12/12", revenue: 100 },
+    { date: "13/12", revenue: 3200 },
+    { date: "14/12", revenue: 900 },
+    { date: "15/01", revenue: 15380 },
+    { date: "22/01", revenue: 3200 },
+    { date: "29/01", revenue: 12200 },
 ]
-export function RevenueChart(){
-    return(
+export function RevenueChart() {
+    return (
         <Card className="col-span-6">
             <CardHeader className="flex-row items-center justify-between pb-8">
                 <div className="space-y-1">
@@ -22,13 +22,15 @@ export function RevenueChart(){
             </CardHeader>
             <CardContent>
                 <ResponsiveContainer width="100%" height={248}>
-                    <LineChart data={data} style={{fontSize:12}}>
+                    <LineChart data={data} style={{ fontSize: 12 }}>
                         <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
-                        <YAxis stroke="#888" axisLine={false} tickLine={false} width={88} tickFormatter={(value:number) => value.toLocaleString("pt-br",{
-                            style:'currency',
-                            currency:'BRL'
-                        }) }/>
+                        <YAxis stroke="#888" axisLine={false} tickLine={false} width={88} tickFormatter={(value: number) => value.toLocaleString("pt-br", {
+                            style: 'currency',
+                            currency: 'BRL'
+                        })} />
+                        <CartesianGrid vertical={false} className="stroke-muted" />
                         <Line type="linear" strokeWidth={2} dataKey="revenue" stroke={colors.violet['500']} />
+                        <Tooltip/>
                     </LineChart>
                 </ResponsiveContainer>
             </CardContent>
