aa90f18f97c04dd379f77b6396ec3f1bb2beb2e6
breno-okra <brenookra@gmail.com>
2024-07-22 20:22:09 -0300

    add axios,envs and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Toaster } from 'sonner'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 export function App() {
 
   return (
@@ -14,8 +16,11 @@ export function App() {
         {/* chamando em cada pagina  <Helmet title='title' /> com seu proprio title */}
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors closeButton />
-        <RouterProvider router={router} />
-     
+        <QueryClientProvider client={queryClient}>
+          {/* o react query facilita requisições com hook do backend, ele memoriza as requisições entre components */}
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+
       </ThemeProvider>
     </HelmetProvider>
 
