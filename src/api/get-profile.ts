b5db72679e4d5856739dd80ff2b3457636862041
breno-okra <brenookra@gmail.com>
2024-07-23 13:18:42 -0300

    using useQuery React-query for recover infos to profile

---
+++ src/api/get-profile.ts
@@ -0,0 +1,23 @@
+import { api } from "@/lib/axios";
+
+interface GetProfileResponse{
+    id:string;
+    name:string;
+    email:string;
+    phone:string | null
+    role: 'manager' | 'customer'
+    createdAt: Date | null
+    updatedAt: Date | null
+}
+export async function getProfile(){
+    const response = await api.get<GetProfileResponse>('/me')
+    return response.data
+}
+/* algumas maneiras de onde incluir a tipagem:
+
+export async function getProfile():Promise<GetProfileResponse>{
+
+    const response = await api.get<GetProfileResponse>('/me')
+
+    return response.data as GetProfileResponse
+} */
