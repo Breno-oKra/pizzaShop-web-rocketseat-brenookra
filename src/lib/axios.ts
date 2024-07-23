f4183055f51086b4f4f995e4b7d14f82726a4070
breno-okra <brenookra@gmail.com>
2024-07-23 14:03:45 -0300

    using api.interceptors for create delay

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,11 @@ export const api = axios.create({
     baseURL: env.VITE_API_URL,
     withCredentials:true
 })
+/* hakcezinho para dar um delay no retorno da api, para simular um loading por exemplo*/
+/* esse codigo tambem serve para customizar dados da requisição, esse config é o retorno, o header,body*/
+if(env.VITE_ENABLE_API_DELAY){
+    api.interceptors.request.use(async(config) => {
+        await new Promise(resolve => setTimeout(resolve,5000))
+        return config
+    })
+}
