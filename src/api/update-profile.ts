eae8278d2648739782abe3770b9e83c71ca09d99
breno-okra <brenookra@gmail.com>
2024-07-23 16:27:21 -0300

    testing interface otimista, simulating errors

--- src/api/update-profile.ts
+++ src/api/update-profile.ts
@@ -6,5 +6,14 @@ export interface UpdateProfileProps{
 
 }
 export async function updateProfile({name,description}:UpdateProfileProps){
+    /* throw new Error()  #simular error */
     await api.put('/profile',{name,description})
-}
+} 
+
+
+/* # simulando um error para teste */
+/* export async function updateProfile({name,description}:UpdateProfileProps){
+    await new Promise((resolve,reject) => {
+        setTimeout(reject,3000)
+    })
+} */
