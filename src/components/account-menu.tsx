ee10aab7ed381b821738914cf09e4e13e3eb0ea2
breno-okra <brenookra@gmail.com>
2024-07-23 15:15:42 -0300

    add modal change profile and using staleTime useQuery

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -6,49 +6,62 @@ import { getProfile } from "@/api/get-profile";
 
 import { getManageRestaurant } from "@/api/get-managed-restaurants";
 import { Skeleton } from "./ui/skeleton";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { StoreProfileDialog } from "./store-profile-dialog";
 
 export function AccountMenu() {
 
     const { data: profile, isLoading: isLoadingProfile } = useQuery({
         queryKey: ['profile'],
-        queryFn: getProfile
+        queryFn: getProfile,
+        staleTime: Infinity
     })
     /* isLoading verifica se uma informação esta sendo carregada, assim não precisamos criar consts com verificações */
+    /* staleTime é usado para atualizar as informações em segundo para caso as informações tenham sido atualizado, mas passamos
+    Infinity para não ser carregado automaticamente e sim manualmente, somente quando pedirmos   
+    */
     const { data: managedRestaurant, isLoading: isLoadingManageRestaurant } = useQuery({
         queryKey: ['manager-restaurant'],
         queryFn: getManageRestaurant,
+        staleTime: Infinity
     })
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" className="flex items-center gap-2 select-none">
-                    {isLoadingManageRestaurant ? (
-                        <Skeleton className="h-4 w-40" />
-                    ) : managedRestaurant?.name}
-                    <ChevronDown className="w-4 h-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel className="flex flex-col">
-                    {isLoadingProfile? (
-                        <div className="space-y-1.5">
-                            <Skeleton className="h-4 w-32"/>
-                            <Skeleton className="h-3 w-24"/>
-                        </div>
-                    ) : <>
-                    <span>{profile?.name}</span>
-                    <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span></>}
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    <Building className="mr-2 h-4 w-4" />
-                    <span>Perfil da Loja</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span>Sair</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Dialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" className="flex items-center gap-2 select-none">
+                        {isLoadingManageRestaurant ? (
+                            <Skeleton className="h-4 w-40" />
+                        ) : managedRestaurant?.name}
+                        <ChevronDown className="w-4 h-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-56">
+                    <DropdownMenuLabel className="flex flex-col">
+                        {isLoadingProfile ? (
+                            <div className="space-y-1.5">
+                                <Skeleton className="h-4 w-32" />
+                                <Skeleton className="h-3 w-24" />
+                            </div>
+                        ) : <>
+                            <span>{profile?.name}</span>
+                            <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span></>}
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem>
+                            <Building className="mr-2 h-4 w-4" />
+                            <span>Perfil da Loja</span>
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+                    <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+                        <LogOut className="mr-2 h-4 w-4" />
+                        <span>Sair</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+                    <StoreProfileDialog/>
+            </DropdownMenu>
+
+        </Dialog>
     )
 }
