a9157dd5cf32a24a1f1125f20409039e02a707af
breno-okra <brenookra@gmail.com>
2024-07-25 16:29:31 -0300

    init tests, with vitest,testing libary,jest and happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import path from "path"
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +11,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
 })
+
---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config'
+
+// https://vitejs.dev/config/
+
+// estamos usando as importações globais para os tests
+ /* 
+  adicionando  test: {
+    globals: true,
+  }, 
+  e adiocionando  "types": ["vitest/globals"] no tsconfig.app.json
+*/
+/*  podemos usar test('1+1 equals 2',() => {
+    expect(1 + 1).toEqual(2)
+  }) */
+// sem precisar importar test, expect etc.
+export default defineConfig({
+    test: {
+        globals: true,
+        setupFiles: ['./test/setup.ts'],
+        //geralmento o vite usa o jsdom que é o mais famoso
+        // ele é capaz de simular um codigo html para que possamos testar os componentes como se
+        // estivessem sendo exibidos em um navegador, mas usaremos o happy-dom por se mais rapido:indicação do curso
+        environment: 'happy-dom'
+    },
+})
