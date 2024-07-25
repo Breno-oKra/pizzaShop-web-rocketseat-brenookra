5c4b9df12001464cb13146f93275b88d98123eac
breno-okra <brenookra@gmail.com>
2024-07-25 18:27:15 -0300

    resolve error React not defined in test spec

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,26 +1,34 @@
 // vitest.config.ts
+import path from "path"
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 
 // estamos usando as importações globais para os tests
- /* 
-  adicionando  test: {
-    globals: true,
-  }, 
-  e adiocionando  "types": ["vitest/globals"] no tsconfig.app.json
+/* 
+ adicionando  test: {
+   globals: true,
+ }, 
+ e adiocionando  "types": ["vitest/globals"] no tsconfig.app.json
 */
 /*  podemos usar test('1+1 equals 2',() => {
     expect(1 + 1).toEqual(2)
   }) */
 // sem precisar importar test, expect etc.
 export default defineConfig({
-    test: {
-        globals: true,
-        setupFiles: ['./test/setup.ts'],
-        //geralmento o vite usa o jsdom que é o mais famoso
-        // ele é capaz de simular um codigo html para que possamos testar os componentes como se
-        // estivessem sendo exibidos em um navegador, mas usaremos o happy-dom por se mais rapido:indicação do curso
-        environment: 'happy-dom'
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    //geralmento o vite usa o jsdom que é o mais famoso
+    // ele é capaz de simular um codigo html para que possamos testar os componentes como se
+    // estivessem sendo exibidos em um navegador, mas usaremos o happy-dom por se mais rapido:indicação do curso
+    environment: 'happy-dom'
+  },
 })
